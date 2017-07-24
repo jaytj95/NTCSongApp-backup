e121ca5cfd7b1c98f952d41b1023991909bae520
Jason <jason@Jasons-MacBook-Pro.local>
2017-07-24 00:52:27 -0700

    List firebase songs in view

--- app/index.js
+++ app/index.js
@@ -1,6 +1,17 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+const {
+    AppRegistry,
+    ListView,
+    StyleSheet,
+    Text,
+    View,
+    TouchableHighlight,
+} = ReactNative;
+import ReactNative from 'react-native';
 import * as firebase from 'firebase';
+import React, {Component} from 'react';
+const StatusBar = require('./components/StatusBar');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js');
 
 // firebase init
 var firebaseConfig = {
@@ -11,12 +22,59 @@ var firebaseConfig = {
     storageBucket: "ntcsongparse.appspot.com",
     messagingSenderId: "955827220203"
 };
-var fireRef = firebase.initializeApp(firebaseConfig);
+var firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default class NTCSongApp extends Component {
+export default class NTCSongApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            })
+        };
+        this.itemsRef = this.getRef().child('song_list');
+    }
+    getRef() {
+        return firebaseApp.database().ref();
+    }
+    componentDidMount() {
+        this.listenForItems(this.itemsRef);
+    }
+    listenForItems(itemsRef) {
+        itemsRef.on('value', (snap) => {
+
+            // get children as an array
+            var items = [];
+            snap.forEach((child) => {
+                items.push({
+                    title: child.child("SongTitle").val(),
+                    _key: child.key
+                });
+            });
+
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(items)
+            });
+
+        });
+    }
+
+    _renderItem(item) {
+        return (
+            <ListItem item={item} onpress=""/>
+        )
+    }
     render() {
         return (
-            <Text>Hello world!</Text>
+            <View style={styles.container}>
+                    <StatusBar title="NTC Song List"/>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this._renderItem.bind(this)}
+                    enableEmptySections={true}
+                    style={styles.listview}/>
+
+            </View>
         );
     }
 }
